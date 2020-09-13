2a1f47c26a35ff4cca9b67419cbf3390bf1db2d3
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2020-09-12 23:01:48 -0300

    Views y htmls creados

--- InfoPermisos/OtroGrupo/views.py
+++ InfoPermisos/OtroGrupo/views.py
@@ -3,3 +3,14 @@ from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	return render(request, 'index.html', {})
+
+
+def login(request):
+	return render(request, 'login.html', {})
+
+
+def perfil(request):
+	return render(request, 'perfil.html', {})
