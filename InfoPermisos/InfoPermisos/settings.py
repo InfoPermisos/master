bce1951f580040c88d8d2588363b1e81325278a9
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2020-09-21 16:02:18 -0300

    Update settings.py
    'crispy_forms' repetido en INSTALLED_APPS = [ ]

--- InfoPermisos/InfoPermisos/settings.py
+++ InfoPermisos/InfoPermisos/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'OtroGrupo.apps.OtrogrupoConfig',
     'crispy_forms',
     'django.forms',
