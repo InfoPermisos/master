3ce0481f2960fe4be9ca7dd5873c382882b7db19
Kevin Leguiza <NicolasKevinLeguiza@gmail.com>
2020-09-21 17:20:45 -0300

    incluyendo el url volver al login

--- InfoPermisos/OtroGrupo/urls.py
+++ InfoPermisos/OtroGrupo/urls.py
@@ -2,21 +2,10 @@ from django.urls import path
 from OtroGrupo import views
 
 urlpatterns = [
-<<<<<<< HEAD
-	#URLS generales
-    path("home/", views.home),
-    path("login/", views.login),
-    path("register/", views.register),
-    #URLS del permisor
-    path("permisor/", views.permisor_general),
-    path("permisor/revision/", views.permisor_revision),
-    path("permisor/masDetalles/", views.permisor_masDetalles),
-    #URLS
     path("estado/", views.estado),
     path("perfil/", views.perfil),
     path("solicitud/", views.solicitud),
     path("historial/", views.historial),
-=======
     path("home/", views.home, name='home'),
     path("login/", views.login, name= 'login'),
     path("register/", views.register, name= 'register'),
@@ -25,5 +14,4 @@ urlpatterns = [
     path("permisor/", views.permisor_general, name= 'permisor'),
     path("permisor/revision/", views.permisor_revision, name= 'permisor_revision'),
     path("permisor/masDetalles/", views.permisor_masDetalles, name= 'permisor_detalles'),
->>>>>>> 1f5e0be04b6758cc75a1f95b7ebe672d458a58fb
 ]
