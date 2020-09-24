28a41008dd305426e750416d91d31be6e45da0ac
Kevin Leguiza <NicolasKevinLeguiza@gmail.com>
2020-09-24 12:53:25 -0300

    Implementacion de recursos y templates

--- InfoPermisos/OtroGrupo/forms.py
+++ InfoPermisos/OtroGrupo/forms.py
@@ -1,80 +1,15 @@
 from django import forms
 from django.forms import ModelForm
-from OtroGrupo.models import Ciudadano, Administrador, Permiso
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from OtroGrupo.models import Ciudadano
 
-# =========================LOGIN======================================================
+# Este form se inserta en perfil_form.html
 
-class MiAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(
-		label='',
-		widget= forms.EmailInput(
-			attrs={
-				"class":"input-form",
-				"type":"email",
-				"placeholder":"Ingrese su email",
-				"required":"True",
-				}
-			)
-		)
-    
-    password = forms.CharField(
-    	label='',
-    	widget=forms.PasswordInput(
-    		attrs={
-				"class":"input-form",
-				"type":"password",
-				"placeholder":"Ingrese su clave",
-			}
-    	)
-    )
-# ================================REGISTRO=================================================
-
-class UserFormExtras(UserCreationForm):
-	# Lista de tuplas, donde cada tupla representa un "choice".
-	genero_choices = [
-		('Masculino', 'Masculino'),
-		('Femenino', 'Femenino'),
-		('Otro', 'Otro')
-	]
-	username = forms.EmailField(label="Email")
-	nombre = forms.CharField(label="Nombre")
-	apellido = forms.CharField(label="Apellido")
-	genero = forms.ChoiceField(label="Género", choices=genero_choices)
-	dni = forms.IntegerField(label="DNI")
-	fecha_nac = forms.DateField(label="Fecha de nacimiento", widget=forms.SelectDateWidget(years=range(1900, 2100)))
-
-	class Meta:
-		model = User
-		fields = ["username", "password1", "password2", "nombre", "apellido", "genero", "dni", "fecha_nac"]
-
-
-class CiudadanoForm(ModelForm):
+class CiudadanoForm(forms.ModelForm):
+	
 	class Meta:
 		model = Ciudadano
-		fields = ['id', 'dniId', 'fechaNacimiento', 'ciudad', 'empleo', 'enfermedades', 'isopado', 'fechaIsopado']
-		widgets = {
-
-		}
-
-
-class AdministradorForm(ModelForm):
-	class Meta:
-		model = Administrador
-		fields = ['id', 'dniId']
-		widgets = {
-
-		}
-
-
-
-class PermisoForm(ModelForm):
-	class Meta:
-		model = Permiso
-		fields = ['id', 'ciudadanoId', 'administradorId', 'tipoPermiso', 'estado', 'detalle', 'motivo']
-		widgets = {
-
-		}
-
+		fields = (
+			'email', 'first_name', 'last_name', 'dni',
+			'fecha_nacimiento', 'genero', 'ciudad', 'empleo',
+			'enfermedades', 'isopado', 'fecha_isopado'
+		)
--- InfoPermisos/OtroGrupo/views.py
+++ InfoPermisos/OtroGrupo/views.py
@@ -3,77 +3,100 @@ from django.contrib.auth import logout as hacer_logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as hacer_login
-from .forms import UserFormExtras, MiAuthenticationForm 
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required # Para filtrar por tipo de usuario: aprobador de permisos o solicitante.
 from django.contrib.auth.models import User, Group
 
-#from django.shortcuts import render
+#Recursos de la misma app
 
-#from django.http import HttpResponse
+from OtroGrupo.models import Ciudadano
+from OtroGrupo.forms import CiudadanoForm
 
-#from appusuario.models import Usuario
 
-#from appusuario.form import FormProducto
-
-#from django.shortcuts import redirect
-
-# Create your views here.
 
 def home(request):
 	return render(request, 'home.html', {})
 
-# =========== View de Login ===========
+# =========== View de Login ======================================================
 def login(request):
-    # Creamos el formulario de autenticación vacío
-	form = MiAuthenticationForm()
+	form = AuthenticationForm()
 	if request.method == "POST":
-        # Añadimos los datos recibidos al formulario
-		form = MiAuthenticationForm(data=request.POST)
-        # Si el formulario es válido...
+		form = AuthenticationForm(data=request.POST)
 		if form.is_valid():
-            # Recuperamos las credenciales validadas
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
-
-            # Verificamos las credenciales del usuario
-			user = authenticate(username=username, password=password)
-
-            # Si existe un usuario con ese nombre y contraseña
+			usuario = form.cleaned_data['username']
+			contrasenia = form.cleaned_data['password']
+			user = authenticate(username=usuario, password=contrasenia)
 			if user is not None:
-                # Hacemos el login manualmente
 				hacer_login(request, user)
-				# Creamos una lista de usuarios que estén en el grupo 'Permisor'.
 				users_in_group = Group.objects.get(name="Permisor").user_set.all()
 				if user in users_in_group:
 					return redirect('permisor') # Si el usuario está en esa lista, te redirecciona a la vista permisor.
                 # Y le redireccionamos al perfil de usuario.
 				else:
 					return redirect('perfil')
+				hacer_login(request, user)
+				return redirect('/')
 
-    # Si llegamos al final renderizamos el formulario
-	return render(request, "login.html", {'form': form})
+	return render(request, "login.html", {'form':form})
 
-
+#===================================================================================
 
 def perfil(request):
-	return render(request, 'perfil.html', {})
 	
-# ============== View de Registro ===================
+	user = request.user
+	if user.is_authenticated:
+		return render(request, "perfil.html", {})
+	else:
+		return redirect("login")
+
+	return render(request, "perfil.html", {})
+
+
+def perfil_form(request):
+
+	if request.user.is_authenticated:
+		form = CiudadanoForm()
+		if request.method == "POST":
+			form = CiudadanoForm(request.POST)
+			if form.is_valid():
+				c_form = form.save(commit=False)
+				c_form.user = request.user
+				form.save()
+				return redirect('detalles_guardados')
+		else:
+			form = CiudadanoForm()
+
+		return render(request, "perfil_form.html", {'form':form})
+
+	return redirect('login')
+	
+# ============== View de Registro ========================
 def register(request):
-	form = UserFormExtras()
+	form = UserCreationForm()
 	form.fields['username'].help_text = None
 	form.fields['password1'].help_text = None
 	form.fields['password2'].help_text = None
 	if request.method == "POST":
-		form = UserFormExtras(data=request.POST)
+		form = UserCreationForm(data=request.POST)
 		if form.is_valid():
 			user = form.save()
 			if user is not None:
-				#hacer_login(request, user)
-				return redirect('login')
+				hacer_login(request, user)
+				return redirect('/')
 	return render(request, "register.html", {'form':form})
+
+# =========================================================
+def logout(request):
+	hacer_logout(request)
+
+	return redirect('home')
+
+# =========================================================
+
+def detalles_guardados(request):
+
+	return render(request, 'detalles_guardados.html', {})
 
 
 def registro_exitoso(request):
@@ -95,8 +118,8 @@ def permisor_revision(request):
 def permisor_masDetalles(request):
 	return render(request, 'permisor_masDetalles.html', {})
 
-def estado(request):
-	return render(request, 'estado.html', {})
-
 def historial(request):
 	return render(request, 'historial.html', {})
+
+def estado(request):
+	return render(request, 'estado.html', {})
